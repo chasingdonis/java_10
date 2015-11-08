044772ea1bee09bd444bdeaa1ee2f8002c2c3c14
chasingdonis <cuixianning@hotmail.com>
2015-11-08 14:55:45 -0600

    edited js, added icon

--- main.js
+++ main.js
@@ -3,15 +3,20 @@ $('a').click(function(){
 		$(this).addClass('changeColor');
 });
 
-$(".title:contains('Lorem')").click(function(e){
+$("li#sidebar_content_1").click(function(e){
 
-    e.preventDefault();
-    var $this = $(this).parent().find('.entry');
-    $(".h2 entry").not($this).hide();
-
+    var $this = $(this).parent().find('li#sidebar_content_1 ul');
     $this.toggle();
-
 });
+    
+$("#content div.post:first h2").click(function(){ 
+$("#content div.post:first div.entry").html('<p>I try to see what will happen</p>');
+    });
+    
+$('div#menu ul li a').prepend('<img id="image" src="icon.png" />')
+
+$('li#sidebar_content_1 ul li:first a').css('color', 'blue');
+$('li#sidebar_content_1 ul li:odd').css('padding-left', 5);
 });
 
 
